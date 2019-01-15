c62f56e4e81b1de6d3df23d38549e946c25b3470
Miguel Santos Batista <rawnnnnn@gmail.com>
2019-01-15 23:55:27 +0100

    Testing hibernate

---
+++ src/main/java/es/migsanbat/onanismo/commands/meta/TestDB.java
@@ -0,0 +1,39 @@
+package es.migsanbat.onanismo.commands.meta;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+import es.migsanbat.onanismo.BotLauncher;
+import es.migsanbat.onanismo.domain.Onanismo;
+import es.migsanbat.onanismo.util.HibernateUtil;
+
+import com.jagrosh.jdautilities.command.Command.Category;
+
+public class TestDB extends Command {
+
+	public TestDB(Category meta) {
+		this.name = "testDB";
+		this.category = meta;
+		this.aliases = new String[] { "db" };
+		this.help = "Comando para comprobar la disponibilidad de la db";
+		this.guildOnly=false;
+	}
+
+	@Override
+	protected void execute(CommandEvent event) {
+		List lista = this.list();
+		Onanismo onanismo=(Onanismo) lista.get(0);
+		event.reply("Onanismo: "+onanismo.toString());
+	}
+	private List list() {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        List result = session.createQuery("from Onanismo").list();
+        session.getTransaction().commit();
+        return result;
+    }
+}
