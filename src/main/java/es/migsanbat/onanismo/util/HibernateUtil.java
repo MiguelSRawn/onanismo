6c9d91800daae1bf9b0ba619fab240fe821b2754
Miguel Santos Batista <rawnnnnn@gmail.com>
2019-01-20 18:55:57 +0100

    Update HibernateUtil.java

--- src/main/java/es/migsanbat/onanismo/util/HibernateUtil.java
+++ src/main/java/es/migsanbat/onanismo/util/HibernateUtil.java
@@ -1,20 +1,32 @@
 package es.migsanbat.onanismo.util;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
 	 private static final SessionFactory sessionFactory = buildSessionFactory();
+	 private static ServiceRegistry serviceRegistry;
 	 
 	    private static SessionFactory buildSessionFactory() {
 	        try {
 	            // Create the SessionFactory from hibernate.cfg.xml
-	        	Configuration config =new Configuration().configure();
-	        	config.setProperty("connection.url", System.getenv("DATABASE_URL"));
-	        	System.out.println("Conectando a "+config.getProperty("connection.url"));
-	            return config.buildSessionFactory();
+	        	Configuration configuration = new Configuration();
+	            configuration.configure();
+	            Map<String,String> jdbcUrlSettings = new HashMap<>();
+	            String jdbcDbUrl = System.getenv("DATABASE_URL");
+	            if (null != jdbcDbUrl) {
+	              jdbcUrlSettings.put("hibernate.connection.url", System.getenv("JDBC_DATABASE_URL"));
+	            }
+	            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").applySettings(
+	            		jdbcUrlSettings).build();
+	            System.out.println("Conectando a "+configuration.getProperty("connection.url"));
+	            return configuration.buildSessionFactory(serviceRegistry);
 	        }
 	        catch (Throwable ex) {
 	            // Make sure you log the exception, as it might be swallowed
