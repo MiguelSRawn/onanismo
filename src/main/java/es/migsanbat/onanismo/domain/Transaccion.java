417bd4f3fa409318b50227a0d82ee42e2990459d
Miguel Santos Batista <rawnnnnn@gmail.com>
2019-01-21 02:19:14 +0100

    Update Transaccion.java

--- src/main/java/es/migsanbat/onanismo/domain/Transaccion.java
+++ src/main/java/es/migsanbat/onanismo/domain/Transaccion.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -24,9 +25,9 @@ public class Transaccion implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private Long id;
-	@OneToMany(mappedBy="entrante")
+	@ManyToOne()
 	private User beneficiario;
-	@OneToMany(mappedBy="saliente")
+	@ManyToOne()
 	private User benefactor;
 	@Column(name="balanza")
 	private Long balanza;
