26de8759bc2b550fc84065164a2490cc1f2a0a7a
Miguel Santos Batista <rawnnnnn@gmail.com>
2019-01-21 14:03:47 +0100

    user help command

--- src/main/java/es/migsanbat/onanismo/commands/user/UserCommand.java
+++ src/main/java/es/migsanbat/onanismo/commands/user/UserCommand.java
@@ -5,6 +5,7 @@ import com.jagrosh.jdautilities.command.CommandEvent;
 
 import es.migsanbat.onanismo.domain.User;
 import es.migsanbat.onanismo.services.UserService;
+import es.migsanbat.onanismo.util.BotUtil;
 
 import com.jagrosh.jdautilities.command.Command.Category;
 
@@ -23,15 +24,33 @@ public class UserCommand extends Command {
 		String[] args = event.getArgs().split(" ");
 		User user;
 		String reply ="";
-		if(args.length>.0) {
+		if(args.length>0) {
 			switch(args[0]) {
 			case "":
-				reply = "Escribe 'user' help para ver la lista de comandos de user";
+				reply = "Escribe '+user help' para ver la lista de comandos de user";
 				break;
 			case "help":
+				reply = BotUtil.get().commandHelp(BotUtil.USER_COMMAND, args);
 				break;
 			case "see":
 			case "ver":
+				String discordId;
+				if(args.length>1) {
+					discordId = args[1];
+				}else {
+					discordId = event.getAuthor().getId();
+				}
+				user = UserService.get().findOneByDiscordId(discordId);
+				if(user==null) {
+					reply = "Usuario inexistente.";
+				}else {
+					reply = "Usuario encontrado: \n"
+							+ "		Nombre: "+user.getName()+"\n"
+							+ "		ID: "+user.getDiscordId()+"\n"
+							+ "		Saldo: "+user.getCartera().getSaldoPropio()+"\n"
+							+ "		Onanismos: "+user.getOnanismos().size()+"\n"
+							+ "Escribe +help para ver la lista de comandos";
+				}
 				break;
 			case "ingresar":
 			case "añadir":
--- src/main/java/es/migsanbat/onanismo/util/BotUtil.java
+++ src/main/java/es/migsanbat/onanismo/util/BotUtil.java
@@ -4,11 +4,20 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import javax.swing.plaf.ButtonUI;
+
+import org.omg.PortableInterceptor.USER_EXCEPTION;
+
 import net.dv8tion.jda.core.events.ReadyEvent;
 
 public class BotUtil {
-private static BotUtil instancia;
-private static String ownerId =System.getenv("OWNER_ID");
+	private static BotUtil instancia;
+	private static String ownerId =System.getenv("OWNER_ID");
+	
+	public static final int USER_COMMAND = 0;
+	public static final int BALANCE_COMMAND = 1;
+	public static final int TESTDB_COMMAND = 2;
+	public static final int PING_COMMAND = 3;
 	
 	public static BotUtil get() {
 		if(instancia==null) {
@@ -29,5 +38,18 @@ private static String ownerId =System.getenv("OWNER_ID");
 	    for(Object o: c)
 	      r.add(clazz.cast(o));
 	    return r;
+	}
+	
+	public String commandHelp(int command,String[] args) {
+		String reply = "";
+		switch(command) {
+			case USER_COMMAND:
+				reply = "Type: \n"
+						+ "'+user help': Es este comando! \n"
+						+ "'+user ver': Mostrar la información de tu usuario o el usuario dado \n"
+						+ "'+user crear': Crea un nuevo usuario si no tienes ya uno";
+				break;
+		}
+		return reply;
 	}
 }
