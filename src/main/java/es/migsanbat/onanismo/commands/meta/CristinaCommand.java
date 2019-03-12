ca7ec7304d77efae2977a16d6ae50633e2cbcbae
Miguel Santos Batista <rawnnnnn@gmail.com>
2019-03-12 13:59:49 +0100

    Update CristinaCommand.java

--- src/main/java/es/migsanbat/onanismo/commands/meta/CristinaCommand.java
+++ src/main/java/es/migsanbat/onanismo/commands/meta/CristinaCommand.java
@@ -23,14 +23,21 @@ public class CristinaCommand extends Command {
 
 	@Override
 	protected void execute(CommandEvent event) {
+		String reply = "";
 		List<Onanismo> onas =UserService.get().findOneByDiscordId("543440071567736832").getOnanismos();
 		int[] array =new int[7];
 		for(Onanismo ona:onas) {
 			array[ona.getFecha().toInstant().atZone(ZoneId.of("UTC+1")).toLocalDate().getDayOfWeek().getValue()-1]++;
-		}		
-		for(int i=0;i<7;i++) {
-			event.reply(DayOfWeek.of(i+1).toString()+": "+array[i]);
 		}
+		reply+="Pablo: "+array[0]+"\n";
+		reply+="Agus: "+array[1]+"\n";
+		reply+="Dani: "+array[2]+"\n";
+		reply+="RafaG: "+array[3]+"\n";
+		reply+="Andy: "+array[4]+"\n";
+		reply+="Miguel: "+array[5]+"\n";
+		reply+="Sunday: "+array[6]+"\n";
+		
+		event.reply(reply);
 	}
 
 }
